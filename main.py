816651aa5fe7bc7c415965fc4311342192d45d89
mariestigen <marie.stigen@hotmail.com>
2021-06-20 16:01:09 +0200

    use python instead of flask for init

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+port = int(os.environ.get('PORT', 9999))
+print (port)
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, Cats!</p>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
