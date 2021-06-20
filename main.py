4e3520812a28f85f497db52e78c5d7b69d199909
mariestigen <marie.stigen@hotmail.com>
2021-06-20 16:25:02 +0200

    Got HTML up and running. Added static and updated main

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, request, send_from_directory
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 port = int(os.environ.get('PORT', 9999))
-print (port)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, Cats!</p>"
+def send_html():
+    return send_from_directory('static/html', "index.html")
 
+#LAST LINE
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
